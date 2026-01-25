35fa760703524afc42755f191658310be03dcd9c
Kyle Peterson <94805808+kp2485@users.noreply.github.com>
2026-01-24 19:50:34 -0500

    conversion between directions and coordinates

--- DungeonCrawler/Coordinate.swift
+++ DungeonCrawler/Coordinate.swift
@@ -6,8 +6,16 @@
 //
 
 struct Coordinate {
-    let x: Int
-    let y: Int
+    var x: Int
+    var y: Int
+    
+    static func +=(coordinate: inout Coordinate, direction: Direction) {
+        coordinate += direction.toCoordinate
+    }
+    
+    static func +=(lhs: inout Coordinate, rhs: Coordinate) {
+        lhs.y += rhs.y
+    }
 }
 
 extension Coordinate: Equatable { }
--- DungeonCrawlerTests/DungeonCrawlerTests.swift
+++ DungeonCrawlerTests/DungeonCrawlerTests.swift
@@ -10,11 +10,23 @@ import Testing
 
 @Suite("Player movement should")
 struct PlayerMovementTests {
-    @Test("move forward when we instruct the player to move forward") func PlayerMoveForward() {
+    @Test("movement in a specified direction gets to the expected coordinate", arguments: [
+        (Direction.forward, Coordinate(x: 0, y: 1)),
+        (Direction.backwards, Coordinate(x: 0, y: -1))
+    ]) func PlayerMoveForward(testcase: (direction: Direction, expectedPosition: Coordinate)) {
+        let player = Player()
+        
+        player.move(testcase.direction)
+        
+        #expect(player.position == testcase.expectedPosition)
+    }
+    
+    @Test("move forward twice to get to coordinate (0,2)") func PlayerMoveForwardTwice() {
         let player = Player()
         
         player.move(.forward)
+        player.move(.forward)
         
-        #expect(player.position == Coordinate(x:0, y:1))
+        #expect(player.position == Coordinate(x:0, y:2))
     }
 }
